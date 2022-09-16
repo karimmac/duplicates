7c386d547baf8c02b69358ff3efee281ca405d87
Karim MacDonald <karim.macdonald@gmail.com>
2022-09-16 23:07:40 +0300

    Issue-4 Read/write dupes to/from CSV/JSON files

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 ## Issues
 
-| #   | State  | Title                                      |
-| --- | ------ | ------------------------------------------ |
-| 5   | CLOSED | Write found duplicates to a CSV file       |
-| 4   | OPEN   | Load duplicates file for processing        |
-| 3   | OPEN   | Write found duplicates to a file           |
-| 2   | OPEN   | List duplicates in multiple directories    |
-| 1   | OPEN   | List duplicate files in a single directory |
+| # | State  | Title                                      |
+|---|--------|--------------------------------------------|
+| 5 | CLOSED | Write found duplicates to a CSV file       |
+| 4 | CLOSED | Load duplicates file for processing        |
+| 3 | CLOSED | Write found duplicates to a file           |
+| 2 | OPEN   | List duplicates in multiple directories    |
+| 1 | CLOSED | List duplicate files in a single directory |
--- duplicates.py
+++ duplicates.py
@@ -12,12 +12,12 @@ from pathlib import Path
 import find_duplicate_files
 
 
-def _output_dupes_json(dupes, out_file):
+def _output_dupes_json(dupes: list, out_file: Path):
     with out_file.open('w') as dupes_file:
         json.dump(dupes, dupes_file)
 
 
-def _output_dupes_csv(dupes, out_file):
+def _output_dupes_csv(dupes: list, out_file: Path):
     dupe_lengths = list(map(len, dupes))
     dupes_with_counts = [[i[0]] + i[1] for i in list(zip(dupe_lengths, dupes))]
     header = ['Count'] + ['Path'] * max(dupe_lengths)
@@ -27,7 +27,7 @@ def _output_dupes_csv(dupes, out_file):
         csv_writer.writerows(dupes_with_counts)
 
 
-def _output_dupes(dupes, out_file, out_type):
+def _output_dupes(dupes: list, out_file: Path, out_type: str):
     out_fn = {
         'CSV': _output_dupes_csv,
         'JSON': _output_dupes_json,
@@ -35,27 +35,64 @@ def _output_dupes(dupes, out_file, out_type):
     out_fn[out_type](dupes, out_file)
 
 
-def _print_dupes(dupes):
+def _print_dupes(dupes: list):
     for i in [[f'"{f}"' for f in row] for row in dupes]:
         print(' '.join(i))
+
+
+def _read_dupes_csv(in_file: Path):
+    with in_file.open('r') as dupes_file:
+        return [i[1:] for i in csv.reader(dupes_file)][1:]
+
+
+def _read_dupes_json(in_file: Path):
+    with in_file.open('r') as dupes_file:
+        return json.load(dupes_file)
+
+
+def _read_dupes(in_file: Path, in_type: str):
+    in_fn = {
+        'CSV': _read_dupes_csv,
+        'JSON': _read_dupes_json,
+    }
+    return in_fn[in_type](in_file)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--search_dir', '-d', help='Directory to search for duplicates')
+    parser.add_argument('--in_file', '-i', help='Input file path', default=None)
+    parser.add_argument('--in_type', '-it', help='Input file type (default: JSON)',
+                        choices=['CSV', 'JSON'], default='JSON')
+    parser.add_argument('--out_file', '-o', help='Output file path', default=None)
+    parser.add_argument('--out_type', '-t', help='Output file type (default: CSV)',
+                        choices=['CSV', 'JSON'], default='CSV')
+    return parser.parse_args()
+
+
+def _find_dupes(search_dir_path: str):
+    search_dir = Path(search_dir_path)
+    if not search_dir.is_dir():
+        raise RuntimeError(f'{search_dir} is not a directory')
+
+    return find_duplicate_files.find_duplicate_files(search_dir, chunks=2)
 
 
 def main():
     """
     Find and manage duplicate files.
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--search_dir', '-d', help='Directory to search for duplicates', required=True)
-    parser.add_argument('--out_file', '-o', help='Output file path', default=None)
-    parser.add_argument('--out_type', '-t', help='Output file type (default: CSV)',
-                        choices=['CSV', 'JSON'], default='CSV')
-    args = parser.parse_args()
+    args = _parse_args()
 
     out_file = Path(args.out_file) if args.out_file else None
     if out_file and out_file.exists():
         raise RuntimeError(f'{out_file} already exists')
 
-    dupes = find_duplicate_files.find_duplicate_files(args.search_dir, chunks=2)
+    dupes = None
+    if args.in_file:
+        dupes = _read_dupes(Path(args.in_file), args.in_type)
+    else:
+        dupes = _find_dupes(args.search_dir)
 
     if out_file:
         _output_dupes(dupes, out_file, args.out_type)
